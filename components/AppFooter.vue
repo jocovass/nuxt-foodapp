992955a8a175a0f4f3a4799cb48837ae59b3daaa
JozsefVass <joco.udv@gmail.com>
2021-09-29 07:03:13 +0100

    Initial commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="footer">
+    <p>Copirite information goes here.</p>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/restaurants.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="container restaurant">
+    <header>
+      <h1>Restaurants</h1>
+    </header>
+    <AppRestaurantInfo />
+  </main>
+</template>
+
+<script>
+import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue'
+
+export default {
+  components: {
+    AppRestaurantInfo,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  padding: 20px 200px;
+  margin-top: 50px;
+}
+</style>
